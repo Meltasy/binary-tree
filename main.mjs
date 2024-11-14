051348722c8e0b1469e7c117f13367fb1c68197f
Meltasy <119603631+Meltasy@users.noreply.github.com>
2024-11-14 16:47:52 +0100

    Add methods to binary tree using js
    Create methods to insert, delete and find nodes
    Test the methods to ensure they work

--- main.mjs
+++ main.mjs
@@ -1,22 +1,26 @@
 import { Tree } from './tree.mjs'
 
-const test = new Tree
-const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-const sortedArray = test.filterArray(testArray)
+const test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
-      return
-    }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false)
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`)
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true)
-    }
-  };
+  if (node === null) {
+    return
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false)
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`)
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true)
+  }
+}
 
-console.log(test.filterArray(testArray))
-console.log(test.buildTree(sortedArray))
-console.log(prettyPrint(test.buildTree(sortedArray)))
+console.log(test.root)
+// console.log(prettyPrint(test.root))
+console.log(test.insert(6))
+console.log(test.insert(32))
+console.log(test.delete(8))
+console.log(test.delete(4))
+console.log(prettyPrint(test.root))
+console.log(test.find(6))
+console.log(test.find(67))
--- tree.mjs
+++ tree.mjs
@@ -3,20 +3,84 @@ import { Node } from './node.mjs'
 class Tree {
   constructor(array) {
     this.array = array
-    this.root = null
-  }
-  filterArray(array) {
-    return [...new Set(array.sort((a, b) => a - b))]
+    this.root = this.buildTree(array)
   }
   buildTree(array) {
-    if (array.length === 0) {
+    let uniqueArray = [...new Set(array)]
+    let sortedArray = uniqueArray.sort((a, b) => a - b)
+    if (sortedArray.length === 0) {
       return null
     }
-    let middle = Math.floor(array.length / 2)
-    let root = new Node(array[middle])
-    root.left = this.buildTree(array.slice(0, middle))
-    root.right = this.buildTree(array.slice(middle + 1))
+    let middle = Math.floor(sortedArray.length / 2)
+    let node = new Node(sortedArray[middle])
+    node.left = this.buildTree(sortedArray.slice(0, middle))
+    node.right = this.buildTree(sortedArray.slice(middle + 1))
+    return node
+  }
+  insert(data, root = this.root) {
+    if (!root.data) {
+      return new Node(data)
+    }
+    if (data < root.data) {
+      if (!root.left) {
+        root.left = new Node(data)
+      } else {
+        this.insert(data, root.left)
+      }
+    } else {
+      if (!root.right) {
+        root.right = new Node(data)
+      } else {
+        this.insert(data, root.right)
+      }
+    }
     return root
+  }
+  delete(data) {
+    this.root = this.deleteNode(data, this.root)
+  }
+  getSuccessor(current) {
+    current = current.right
+    while (current && current.left) {
+      current = current.left
+    }
+    return current
+  }
+  deleteNode(data, root) {
+    if (!root) {
+      return root
+    }
+    if (data === root.data) {
+      if (!root.left && !root.right) {
+        return null
+      } else if (!root.left) {
+        return root.right
+      } else if (!root.right) {
+        return root.left
+      } else {
+        let successor = this.getSuccessor(root)
+        root.data = successor.data
+        root.right = this.deleteNode(successor.data, root.right)
+      }
+    } else if (data < root.data) {
+      root.left = this.deleteNode(data, root.left)
+    } else {
+      root.right = this.deleteNode(data, root.right)
+    }
+    return root
+  }
+  find(data) {
+    let current = this.root
+    while (current !== null) {
+      if (data === current.data) {
+        return current
+      } else if (data < current.data) {
+        current = current.left
+      } else {
+        current = current.right
+      }
+    }
+    return current
   }
 }
 
